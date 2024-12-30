364f10ac1c62d39ba779245ad34955b32cf0ad26
Felipe Lohan <felipelohan03052006@gmail.com>
2024-12-30 16:06:56 -0300

    Cleaned App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import styled from "styled-components";
-import { CategoryCard } from "./components/CategoryCard";
-import { CtaButton } from "./components/CtaButton";
-import { CtaLoadMore } from "./components/CtaLoadMore";
 import { HeaderClient } from "./components/HeaderClient";
-import { LoginCard } from "./components/LoginCard";
-import { ProductCatalogCard } from "./components/ProductCatalogCard";
-import { ProductDetailsCard } from "./components/ProductDetailsCard";
-import { SearchInput } from "./components/SearchInput";
-import { ProductDetailsInCart } from "./components/ProductDetailInCart";
+
 
 function App() {
   return (
     <>
       <HeaderClient />
-      <ProductDetailsInCart />
     </>
   );
 }
