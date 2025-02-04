236232a32b8781bde99099fb1c9a7e695461a23f
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-04 10:11:25 -0300

    acess-token-repository created

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.celestialcommerce/Cart";
+export const TOKEN_KEY = "com.celestialcommerce/Token"
 
 export const CLIENT_ID = "myclientid";
 
