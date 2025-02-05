2f80e9ccc7d53129c7e8184c528e62068ae0853b
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-05 10:27:15 -0300

    isAuthenticated working

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,38 +1,56 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
-import * as accessTokenRepository from "../localstorage/access-token-repository.ts"
+import * as accessTokenRepository from "../localstorage/access-token-repository.ts";
+import jwtDecode from "jwt-decode";
 
-export function loginRequest(loginData: CredentialsDTO){
-  
+export function loginRequest(loginData: CredentialsDTO) {
   const headers = {
     "Content-Type": "application/x-www-form-urlencoded",
-    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
-  }
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+  };
 
-  const requestBody = QueryString.stringify({...loginData, grant_type: "password"})
-  
+  const requestBody = QueryString.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
+
   const config: AxiosRequestConfig = {
     method: "POST",
     url: "/oauth/token",
     data: requestBody,
-    headers: headers
-  }
-  
+    headers: headers,
+  };
 
-  return requestBackend(config)
+  return requestBackend(config);
 }
 
-export function logout(){
+export function logout() {
   accessTokenRepository.remove();
 }
 
-export function saveAccessToken(token: string){
-  accessTokenRepository.save(token)
+export function saveAccessToken(token: string) {
+  accessTokenRepository.save(token);
 }
 
-export function getAccessToken(){
-  return accessTokenRepository.get()
+export function getAccessToken() {
+  return accessTokenRepository.get();
 }
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+  try {
+    const token = accessTokenRepository.get();
+    return token == null
+      ? undefined
+      : (jwtDecode(token) as AccessTokenPayloadDTO);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export function isAuthenticated(): boolean {
+  let tokenPayload = getAccessTokenPayload();
+  return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+  }
