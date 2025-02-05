7b23f579baca2d110022e69a8f4f061191bbe83c
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-05 11:03:11 -0300

    Updated PrivateRoute using hasAnyRoles

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
             <Route
               path="/admin"
               element={
-                <PrivateRoute>
+                <PrivateRoute roles={["ROLE_ADMIN"]} >
                   <Admin />
                 </PrivateRoute>
               }
--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,12 +1,17 @@
 import { Navigate } from "react-router-dom";
 import * as authService from "../../services/auth-service.ts";
+import { RoleEnum } from "../../models/auth.ts";
 
 type Props = {
   children: JSX.Element;
-  }
-  export function PrivateRoute({ children }: Props) {
+  roles?: RoleEnum[];
+};
+export function PrivateRoute({ children, roles = [] }: Props) {
   if (!authService.isAuthenticated()) {
-  return <Navigate to="/login" />;
+    return <Navigate to="/login" />;
+  }
+  if (!authService.hasAnyRoles(roles)) {
+    return <Navigate to="/catalog" />;
   }
   return children;
-  }
+}
