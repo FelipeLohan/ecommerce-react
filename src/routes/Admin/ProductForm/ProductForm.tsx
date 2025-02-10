2749c8aae83fcccf06309da3425731643f267a7b
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-10 10:17:25 -0300

    maked ProductForm in admin style

--- src/routes/Admin/ProductForm/ProductForm.tsx
+++ src/routes/Admin/ProductForm/ProductForm.tsx
@@ -1,9 +1,83 @@
+import styled from "styled-components";
+
+
+const ProductFormContainer = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  padding: 40px;
+  background-color: #fff;
+`;
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+
+  input {
+  width: 100%;
+    padding: 15px;
+    border: 1px solid #d9d9d9;
+    border-radius: 8px;
+  }
+
+  input::placeholder {
+    color: #d9d9d9;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 20px;
+
+  button{
+    width: 100%;
+    padding: 15px;
+  }
+`
+
+const SaveButton = styled.button`
+  width: 100%;
+    padding: 15px;
+    background-color: #3483FA;
+    color: #fff;
+    border: none;
+    border-radius: 8px;
+    font-size: 2.2vmin;
+    cursor: pointer;
+`
+
+const CancelButton = styled.button`
+  width: 100%;
+    padding: 15px;
+    color: #3483FA;
+    background-color: #fff;
+    border: 1px solid #3483FA;
+    border-radius: 8px;
+    font-size: 2.2vmin;
+    cursor: pointer;
+`
+
 const ProductForm = () => {
-  return(
+  return (
     <>
-    <h1>Formulário de Produtos!</h1>
+      <ProductFormContainer>
+        <FormContainer>
+          <h1>DADOS DO PRODUTO</h1>
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Preço" />
+          <input type="text" placeholder="Imagem" />
+          <input type="text" placeholder="Categorias" />
+          <input type="text" placeholder="Descrição" />
+          <ButtonsContainer>
+            <CancelButton>Cancelar</CancelButton>
+            <SaveButton>Salvar</SaveButton>
+          </ButtonsContainer>
+        </FormContainer>
+      </ProductFormContainer>
     </>
-  )
-}
+  );
+};
 
 export { ProductForm };
