88d28fed4bf48b437323717846f10c7ceea20b24
Felipe Lohan <felipelohan03052006@gmail.com>
2025-02-19 20:06:28 -0300

    Added backend in heroku

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,6 @@
 export const CART_KEY = "com.celestialcommerce/Cart";
 export const TOKEN_KEY = "com.celestialcommerce/Token"
 
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://ecommerce-spring-ec44b57ed84d.herokuapp.com";
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
 export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
